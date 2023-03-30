6ba5dfd809fd4367876cf65c56f8788b2987b6cf
mario2603 <mrod02871@gmail.com>
2023-03-30 18:16:10 -0500

    Server is working and displays pokemon images

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3000
-const Pokemon = require('./models/pokemon.js');
+const pokemon = require('./models/pokemon.js');
 
 // INDEX
 app.get('/', (req, res) => {
-res.render('index.ejs', { data: Pokemon });
+res.render('index.ejs', { data: pokemon });
 });
 
 // SHOW
 app.get('/:id', (req, res) => {
-res.render('show.ejs', { data: Pokemon[req.params.id] });
+res.render('show.ejs', { data: pokemon[req.params.id] });
 });
 //New
-
+app.get
 
 //Edit
-
+app.get('/pokemon/:id/edit', (req,res)=>{
+})
 
 //Create
-
+app.post
 
 //Update
 
 
+
 //Destroy
+app.delete
+
 
 //App Listener
 app.listen(port, ()=>{
