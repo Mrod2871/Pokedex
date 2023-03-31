86e19252b3de398891c67e637f57e1cba652fca0
mario2603 <mrod02871@gmail.com>
2023-03-30 22:12:59 -0500

    Fufills mvp requirements

--- server.js
+++ server.js
@@ -1,36 +1,62 @@
+//Dependencies
 const express = require('express');
+const methodOverride = require('method-override')
 const app = express();
 const port = 3000
 const pokemon = require('./models/pokemon.js');
 
-// INDEX
+
+//
+app.use('/public', express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
+// Index
 app.get('/', (req, res) => {
-res.render('index.ejs', { data: pokemon });
+    res.render('index.ejs', { data: pokemon });
 });
 
-// SHOW
-app.get('/:id', (req, res) => {
-res.render('show.ejs', { data: pokemon[req.params.id] });
-});
 //New
-app.get
+app.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
 
-//Edit
-app.get('/pokemon/:id/edit', (req,res)=>{
+//Destroy
+app.delete('/:id', (req, res)=> {
+    pokemon.splice(req.params.id, 1)
+    res.redirect('/')
+})
+
+//Update
+app.put('/:id', (req, res) => {
+    pokemon[req.params.id] = req.body
+    res.redirect(`/`)
 })
 
 //Create
-app.post
+app.post('/', (req, res)=> {
+    pokemon.push(req.body)
+    res.redirect('/')
+})
 
-//Update
+//Edit
+app.get('/:id/edit', (req,res)=> {
+    res.render('edit.ejs', 
+    {
+        data: pokemon[req.params.id] 
+    })
+})
 
-
-
-//Destroy
-app.delete
-
+// SHOW
+app.get('/:id', (req, res) => {
+res.render('show.ejs', 
+    { 
+        data: pokemon[req.params.id] 
+    });
+});
 
 //App Listener
-app.listen(port, ()=>{
-    console.log("Running..");
+app.listen(port, () => {
+    console.log(`Running on ${port}`);
 })
